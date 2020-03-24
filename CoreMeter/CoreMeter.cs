5ab07fa87754651a010b4236939afad85254cd8a
Shane Duffy <shane@shaneduffy.io>
2020-03-24 13:31:10 -0700

    Implemented position/size locking as well as hiding from windows menus

--- CoreMeter/CoreMeter.cs
+++ CoreMeter/CoreMeter.cs
@@ -10,17 +10,21 @@ namespace CoreMeter
     {
         private int _bottomZOrder = 0;
         private IntPtr _hWnd;
+
         private CancellationTokenSource _cancellationTokenSource;
 
-        public CoreMeterUtility(IntPtr hWnd)
+        public CoreMeterUtility(IntPtr hWnd, bool hideFromAltTabMenu = true)
         {
             _hWnd = hWnd;
             _cancellationTokenSource = new CancellationTokenSource();
+
+            if (hideFromAltTabMenu)
+                HideFromAltTabMenu(_hWnd);
         }
 
-        public async void Lock()
+        public async void Lock(int? x = null, int? y = null, int? width = null, int? height = null)
         {
-            await Lock(_cancellationTokenSource.Token);
+            await Lock(_cancellationTokenSource.Token, x, y, width, height);
         }
 
         public void Unlock()
@@ -30,17 +34,42 @@ namespace CoreMeter
             _cancellationTokenSource = new CancellationTokenSource();
         }
 
-        private async Task Lock(CancellationToken cancellationToken)
+        private async Task Lock(CancellationToken cancellationToken, int? x, int? y, int? width, int? height)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
+                bool update = false;
+
                 int zOrder = GetZOrder(_hWnd);
 
-                Debug.WriteLine(zOrder);
+                RECT windowRect;
+
+                GetWindowRect(_hWnd, out windowRect);
+
+                // Verbosity is a good way to make friends
 
                 if (zOrder != _bottomZOrder)
+                    update = true;
+
+                if (x != null)
+                    if (!windowRect.Left.Equals(x))
+                        update = true;
+
+                if (y != null)
+                    if (!windowRect.Top.Equals(y))
+                        update = true;
+
+                if (width != null)
+                    if (!(windowRect.Right - windowRect.Left).Equals(width))
+                        update = true;
+
+                if (height != null)
+                    if (!(windowRect.Right - windowRect.Left).Equals(height))
+                        update = true;
+
+                if (update)
                 {
-                    SendWindowToBottom(_hWnd);
+                    SendWindowToBottom(_hWnd, x ?? windowRect.Left, y ?? windowRect.Top, width ?? (windowRect.Right - windowRect.Left), height ?? (windowRect.Right - windowRect.Left));
                     _bottomZOrder = GetZOrder(_hWnd);
                 }
 
@@ -55,7 +84,23 @@ namespace CoreMeter
         [DllImport("user32.dll", SetLastError = true)]
         private static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
 
-        int GetZOrder(IntPtr hWnd)
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        private static int GetZOrder(IntPtr hWnd)
         {
             const uint GW_HWNDNEXT = 2;
 
@@ -67,16 +112,27 @@ namespace CoreMeter
             return z;
         }
 
-        private static void SendWindowToBottom(IntPtr hWnd)
+        private static void SendWindowToBottom(IntPtr hWnd, int x, int y, int width, int height)
         {
-            const int SWP_NOMOVE = 0x2;
-            const int SWP_NOSIZE = 0x1;
             const int SWP_SHOWWINDOW = 0x0040;
             const int SWP_NOACTIVATE = 0x0010;
 
             const int HWND_BOTTOM = 0x1;
 
-            SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOACTIVATE);
+            Debug.WriteLine(x);
+
+            SetWindowPos(hWnd, HWND_BOTTOM, x, y, width, height, SWP_SHOWWINDOW | SWP_NOACTIVATE);
+        }
+
+        private static void HideFromAltTabMenu(IntPtr hWnd)
+        {
+            const int GWL_EX_STYLE = -20;
+            const int WS_EX_TOOLWINDOW = 0x00000080;
+
+            const int GWL_STYLE = -16;
+
+            SetWindowLong(hWnd, GWL_EX_STYLE, WS_EX_TOOLWINDOW);
+            SetWindowLong(hWnd, GWL_STYLE, 0);
         }
         #endregion
     }
